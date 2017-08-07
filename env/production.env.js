d801eb5f6739d7614b4ac53c57e57d05e57dc8db
Shannon Johnstone <shannon@shannonjohnstone.com.au>
2017-08-07 11:56:14 +1000

    trying to use compose for prod release

--- env/production.env.js
+++ env/production.env.js
@@ -1,2 +1,3 @@
 process.env.NODE_ENV = 'production'
-process.env.MONGO_URI = 'mongodb://shannonjohnstone:shadow1200@ds157740.mlab.com:57740/bookapi'
+// process.env.MONGO_URI = 'mongodb://shannonjohnstone:shadow1200@ds157740.mlab.com:57740/bookapi'
+process.env.MONGO_URI = 'mongodb://mongo:27017/bookAPI'
